fbccd39d923b4a157367f9b6d71be1f0ea8050cb
tqhb2502 <84978747+tqhb2502@users.noreply.github.com>
2022-05-27 13:21:04 +0700

    Delete My2DGame directory

--- My2DGame/src/tile/Tile.java
+++
@@ -1,8 +0,0 @@
-package tile;
-
-import java.awt.image.BufferedImage;
-
-public class Tile {
-	public BufferedImage image;
-	public boolean collision = false;	// determine you can pass this tile or not, if true -> you can not pass, false -> passable
-}
